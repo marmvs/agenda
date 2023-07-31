40c4ece4a7600ee6943b14f2fbe9bc2b12e021b8
marmvs <marcia2005vs@hotmail.com>
2023-07-31 11:24:06 -0300

    :globe_with_meridians: alteração de porta do servidor

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import app from "./src/app.js";
 import chalk from "chalk";
 
-const port = 2727;
+const port = 8080;
 
 app.listen(port, () => {
   console.log(
-    chalk.blue.brGreen.bold(`Servidor está rodando na porta, ${port}`)
+    chalk.blue.bgMagenta.bold(`Servidor está rodando na porta, ${port}`)
   );
 });
